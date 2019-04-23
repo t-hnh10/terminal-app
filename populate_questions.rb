cda5177c520c52e2d12fb7b78dbb01274fc654ad
t-hnh10 <t.hnh@tuta.io>
2019-04-23 16:42:42 +1000

    added method to randomly select questions

--- populate_questions.rb
+++ populate_questions.rb
@@ -1,15 +1,23 @@
 require_relative './classes/geo_question'
 
+def get_random_questions(questions,number)
+   selected_questions = []
+   number.times do
+      selected_questions << questions.sample
+   end
+   return selected_questions
+end
 
+def get_questions(how_many)
+   questions = [
+      GeoQuestion.new("In which country does edelweiss not grow natively?",
+         ["a) Switzerland", "b) Mongolia", "c) Swaziland", "d) Austria"], "c"),
 
-def get_questions
-   questions = [GeoQuestion.new("In which country does edelweiss not grow natively?",
-   ["a) Switzlerland", "b) Mongolia", "c) Swaziland", "d) Austria"],"c"),
+      GeoQuestion.new("What countries does the equator and Tropic of Capricorn run through?",
+         ["a) Sudan", "b) Saudi Arabia", "c) Chile", "d) Brazil"], "d"),
 
-GeoQuestion.new("What countries does the equator and Tropic of Capricorn run through?",
-    ["a) Sudan", "b) Saudi Arabia", "c) Chille", "d) Brazil"], "d"),
-
-GeoQuestion.new("If you can fit through the nose hole of Great Budha in Japan? Do you:",
-   ["a) have great success","b) enlightenment", "c) great wealth", "d) great health"], "b")]
-   return questions
+      GeoQuestion.new("If you can fit through the nose hole of Great Buddha in Japan? Do you:",
+         ["a) have great success","b) enlightenment", "c) great wealth", "d) great health"], "b")
+   ]
+   return get_random_questions(questions,how_many)
 end

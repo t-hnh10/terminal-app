52f224a3f105e9b8a4ee49dd494c41b8b9a72eb8
t-hnh10 <t.hnh@tuta.io>
2019-04-23 16:41:13 +1000

    implemented class methods and control flow

--- classes/question.rb
+++
@@ -1,9 +0,0 @@
-class GeoQuestion
-    attr_accessor :question, :answer
-    
-    def initialize(question, options, answer)
-        @question = question
-        @options = options
-        @answer = answer
-    end
-end
--- main.rb
+++ main.rb
@@ -1,5 +1,4 @@
 require 'artii'
-
 require_relative './classes/quiz'
 
 # Title screen
@@ -7,6 +6,18 @@ banner = Artii::Base.new
 puts "-------------------------------------WELCOME TO-------------------------------------"
 puts banner.asciify('The Geo Quiz App')
 puts "------------------------------------------------------------------------------------"
-# puts "Please enter your name to begin:"
-# username = gets.chomp
-# puts "\nWelcome #{username}!"
+puts "Please enter your name to begin:"
+player = gets.strip
+puts "\nWelcome #{player}! Enter (q)uit to exit the app at any time."
+
+# Quiz Component
+quiz = Quiz.new(player,3)
+while quiz.quiz_finished? == false do
+    quiz.display_question
+    answer = gets.strip.downcase
+        if answer[0] == "q"
+            break
+        end
+    quiz.check_answer(answer)
+end
+quiz.display_score

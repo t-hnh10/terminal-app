7dea0fbf43ccfdb185b5272173d8e0b6e4b7718b
t-hnh10 <t.hnh@tuta.io>
2019-04-23 15:00:43 +1000

    added file to hold questions

--- populate_questions.rb
+++ populate_questions.rb
@@ -1,6 +1,9 @@
-require_relative 'geo_question'
+require_relative './classes/geo_question'
 
-questions [GeoQuestion.new("In which country does edelweiss not grow natively?",
+
+
+def get_questions
+   questions = [GeoQuestion.new("In which country does edelweiss not grow natively?",
    ["a) Switzlerland", "b) Mongolia", "c) Swaziland", "d) Austria"],"c"),
 
 GeoQuestion.new("What countries does the equator and Tropic of Capricorn run through?",
@@ -8,7 +11,5 @@ GeoQuestion.new("What countries does the equator and Tropic of Capricorn run thr
 
 GeoQuestion.new("If you can fit through the nose hole of Great Budha in Japan? Do you:",
    ["a) have great success","b) enlightenment", "c) great wealth", "d) great health"], "b")]
-
-def get_questions
-    return questions
+   return questions
 end

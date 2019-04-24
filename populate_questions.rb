56e058f11237456db92c22e56a61c49d334d09e4
t-hnh10 <t.hnh@tuta.io>
2019-04-24 15:41:18 +1000

    edited typos in questions

--- populate_questions.rb
+++ populate_questions.rb
@@ -8,23 +8,23 @@ def get_questions(how_many)
       GeoQuestion.new("What countries does the equator and Tropic of Capricorn run through?",
          ["a) Sudan", "b) Saudi Arabia", "c) Chile", "d) Brazil"], "d"),
 
-      GeoQuestion.new("If you can fit through the nose hole of Great Buddha in Japan? Do you:",
-         ["a) have great success","b) enlightenment", "c) great wealth", "d) great health"], "b"),
+      GeoQuestion.new("If you can fit through the nose hole of Great Buddha of Kamakura in Japan? Do you:",
+         ["a) have great success", "b) enlightenment", "c) great wealth", "d) great health"], "b"),
       
-      GeoQuestion.new("On which continent upright-walking ancestors originate?",
-         ["a) Russia", "b) Asia","c)Russia", "d) Africa"],"d"),
+      GeoQuestion.new("On which continent did upright-walking ancestors originate?",
+         ["a) Russia", "b) Asia","c) Russia", "d) Africa"],"d"),
        
-      GeoQuestion.new("How any alternating red and white stripes does the American Flag have?",
+      GeoQuestion.new("How many alternating red and white stripes does the flag of the United States have?",
          ["a) 13", "b) 11", "c) 16 ", "d) 9"], "a"),
       
-      GeoQuestion.new("How sewed the Famous Eureka Flag?",
+      GeoQuestion.new("Who sewed the Famous Eureka Flag?",
          ["a) Anastasia Withers, Anne Duke, Anastasia Hayes", "b) Anne Witherton, Elizabeth Dune, Emily Palsey", "c) Beth Kennedy, Grace King, Annabel Cooke", "d) Anne Holyoake, Mary Basson-Humffray, Anne Ross"], "a"),
       
-      GeoQuestion.new("How many rivers in Russia?",
-         ["a) 200", "b) 40 000", "c) 100 000", "d) 12000"],"c"),
+      GeoQuestion.new("How many rivers are in Russia?",
+         ["a) 200", "b) 40 000", "c) 100 000", "d) 12 000"],"c"),
       
       GeoQuestion.new("Where is Hulun Lake situated?",
-         ["a) Hong Kong", "b) China", "c) Taiwan", "d) Mongolia"], "d"),
+         ["a) Hong Kong SAR", "b) People's Republic of China", "c) Republic of China (Taiwan)", "d) Mongolia"], "b"),
       
       GeoQuestion.new("What is this meaning of the word Canada?",
          ["a) Brown Bears", "b) Good People", "c) Wild", "d) Indian Village"], "d"),
@@ -35,26 +35,26 @@ def get_questions(how_many)
       GeoQuestion.new("Which continent has the world's largest river system",
          ["a) Australia", "b) Asia ", "c) South America", "d) Russia"], "c"),
       
-      GeoQuestion.new("Which Continent is the Great Bear Lake",
+      GeoQuestion.new("Which Continent is the Great Bear Lake located in?",
          ["a) Russia", "b) North America ", "c) Asia", "d) South America"], "b"),
       
-      GeoQuestion.new("Which US city has the largest population of Polish?",
+      GeoQuestion.new("Which US city has the largest Polish population?",
          ["a) Chicago", "b) New York City ", "c) San Fran Cisco", "d) Utah"], "a"),
       
       GeoQuestion.new("The river Liffey flows through the centre of which European city",
          ["a) Lisburn", "b) Dublin", "c) Lillerod ", "d) Copenhagen"], "b"),
       
-      GeoQuestion.new("Which is the world's largest continent?",
-         ["a)Antarctica", "b) Asia","c) Europe", "d) Africa"], "b"),
+      GeoQuestion.new("What is the world's largest continent?",
+         ["a) Antarctica", "b) Asia","c) Europe", "d) Africa"], "b"),
       
       GeoQuestion.new("What razor-thin country accounts for more than half of the western coastline of South America?",
          ["a) Peru", "b) Ecuador","c) Chile ", "d) Bolivia"], "c"),
       
       GeoQuestion.new("What country has the most natural lakes?", 
-         ["a) Australia", "b) India", "c) United State", "d) Canada"], "d"),
+         ["a) Australia", "b) India", "c) United States", "d) Canada"], "d"),
       
       GeoQuestion.new("What is the only sea with coasts?",
-         ["a) Celebes Sea", "b) Sargasso Sea", "c) Adriatic Sea" "d) Mediterranean Sea"], "b"),
+         ["a) Celebes Sea", "b) Sargasso Sea", "c) Adriatic Sea", "d) Mediterranean Sea"], "b"),
       
       GeoQuestion.new("What is the driest place on Earth?",
          ["a) Sahara Desert", "b) Kufra, Libya", "c) Atacama Desert", "d) Mc Murdo, Antica"], "d"), 
@@ -63,7 +63,7 @@ def get_questions(how_many)
          ["a) Peru ", "b) Bolivia",  "c) Chile", "d) Columbia"], "a"),
       
       GeoQuestion.new("Which African nation has the most pyramids?",
-         ["a) Egypt", "b)Sudan", "c) Libya", "d) Algeria"], "b"),
+         ["a) Egypt", "b) Sudan", "c) Libya", "d) Algeria"], "b"),
       
       GeoQuestion.new("What is the oldest city in the world?",
          ["a) Jericho", "b) Athens ", "c) Jerusalem ", "d) Damascus "], "d"),
@@ -75,7 +75,7 @@ def get_questions(how_many)
          ["a) Antarctica", "b) Australia ", "c) South America", "d) Africa"], "b"),
       
       GeoQuestion.new("What is the largest country in the Arabian Peninsula?",
-         [ "a) Jordan", "b) United Arab Emirates", "c) Yemen ", "d) Saudi Arabia"], "d"),
+         ["a) Jordan", "b) United Arab Emirates", "c) Yemen ", "d) Saudi Arabia"], "d"),
       
       GeoQuestion.new("What country has the most coastline?",
          ["a) Canada", "b) United States", "c) China", "d) Russia"], "a"),
@@ -86,10 +86,10 @@ def get_questions(how_many)
       GeoQuestion.new("What country is the home of Kangaroo Island?",
          ["a) Australia", "b) France", "c) Japan", "d) Great Britain"], "a"),
       
-      GeoQuestion.new("What continent in located at latitude 90 degrees south Longitude 0.00 degrees east?",
-         ["a) Asia" "b) Australia", "c) Antarctica", "d) South America"], "c"),
+      GeoQuestion.new("What continent is located at latitude 90 degrees south longitude 0.00 degrees east?",
+         ["a) Asia", "b) Australia", "c) Antarctica", "d) South America"], "c"),
       
-      GeoQuestion.new("What is the larges country in South America?",
+      GeoQuestion.new("What is the largest country in South America?",
          ["a) Columbia", "b) Brazil", "c) Peru", "d) Argentina"], "b"),
       
       GeoQuestion.new("What is the tallest mountain in the world?",
@@ -104,7 +104,7 @@ def get_questions(how_many)
       GeoQuestion.new("What mountain is nicknamed the 'Savage Mountain'?",
          ["a) Annapurna", "b) Matterhorn", "c) Mount Everest", "d) K2"], "d"),
       
-      GeoQuestion.new("Along with Spain and France, What other country has both Atlantic and Mediterranean coastlines?",
+      GeoQuestion.new("Along with Spain and France, what other country has both Atlantic and Mediterranean coastlines?",
          ["a) Egypt", "b) Morocco", "c) Syria", "d) Lebanon"], "b"),
       
       GeoQuestion.new("What country occupies the southern end of the Arabian Peninsula?",
@@ -113,10 +113,10 @@ def get_questions(how_many)
       GeoQuestion.new("What is the tallest mountain in North America?",
          ["a) Mount Rushmore", "b) Denali", "c) King Peak", "d) Mount Steele"], "b"),
       
-      GeoQuestion.new("What mountain is closest to the Moon?",
+      GeoQuestion.new("What mountain is closest to the moon?",
          ["a) Mount Pandim", "b) Mount Everest", "c) Mount Chimborazo", "d) Nanga Parbat"], "c"),
       
-      GeoQuestion.new("What is western most and eastern most states in the United States?",
+      GeoQuestion.new("What are the western and eastern most states in the United States?",
          ["a) Washington & New York", "b) Hawaii & Maine", "c) California & Florida", "d) Alaska & Alaska"], "d"),
       
       GeoQuestion.new("What continent contains the most fresh water?",
@@ -135,7 +135,7 @@ def get_questions(how_many)
          ["a) White Sea", "b) Persian Gulf", "c) Caspian Sea", "d) Baltic Sea"], "a"),
       
       GeoQuestion.new("Which of the following lakes is located in an ancient meteorite impact crater?",
-         ["a) Lake Titicaca", " b) Lake Mweru", "c) Lake Poopo", "d) Lake Bosumtwi"], "d"),
+         ["a) Lake Titicaca", "b) Lake Mweru", "c) Lake Poopo", "d) Lake Bosumtwi"], "d"),
       
       GeoQuestion.new("How many tributaries does the Amazon River have?",
          ["a) 9", "b) 1,100", "c) 3,500", "d) 560"], "b"),
@@ -143,7 +143,7 @@ def get_questions(how_many)
       GeoQuestion.new("What is the oldest active volcano on Earth?",
          ["a) Mount Olympus", "b) Mount Yasur", "c) Mount Etna", "d) Mount Fuji"], "c"),
       
-      GeoQuestion.new("Of the eight major islands in the Hawaiian Archipelago, What is the only one that is uninhabited?",
+      GeoQuestion.new("Of the eight major islands in the Hawaiian Archipelago, what is the only one that is uninhabited?",
          ["a) Molokai", "b) Niihau", "c) Coconut Island", "d) Kaho'Olawe"], "d"),
       
       GeoQuestion.new("What river runs through Paris?",
@@ -192,12 +192,12 @@ def get_questions(how_many)
          ["a) Abuja", "b) Lagos", "c) Ibadan", "d) Kano"], "a"),
       
       GeoQuestion.new("What is the capital of Turkey?",
-         ["a) Istanbul", "b) Izmar", "c) Ankara", "d) Bursa"], "c"),
+         ["a) Istanbul", "b) Izmir", "c) Ankara", "d) Bursa"], "c"),
       
       GeoQuestion.new("Lake Titicaca sits on the border between what two nations?",
          ["a) Ecuador & Columbia", "b) Argentina & Chile", "c) Brazil & Paraguay", "d) Bolivia & Peru"], "d"),
       
-      GeoQuestion.new("What is the largest country in the  world in terms of land area?",
+      GeoQuestion.new("What is the largest country in the world in terms of land area?",
          ["a) China", "b) United States", "c) Russia", "d) Canada"], "c"),
       
       GeoQuestion.new("What U.S. state contains the world's largest flat-top mountain?",

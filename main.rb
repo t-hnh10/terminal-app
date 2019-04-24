2c03042d20cb61c2b9caa89c886727eb9f101187
t-hnh10 <t.hnh@tuta.io>
2019-04-24 15:04:57 +1000

    added slow_puts method and exit banner

--- main.rb
+++ main.rb
@@ -1,14 +1,23 @@
 require 'artii'
 require_relative './classes/quiz'
 
+def slow_puts(string, delay: 1, press_any_key: true)                                           
+    (0..string.size).each do |i|
+        system "clear"
+        puts string[0, i]
+        sleep delay * 0.01
+    end
+end
+
 # Title screen
 banner = Artii::Base.new
+title = 'The Geo Quiz App'
 puts "-------------------------------------WELCOME TO-------------------------------------"
-puts banner.asciify('The Geo Quiz App')
+puts banner.asciify(title)
 puts "------------------------------------------------------------------------------------"
 puts "Please enter your name to begin:"
 player = gets.strip
-puts "\nWelcome #{player}! Enter (q)uit to exit the app at any time."
+slow_puts "\nWelcome #{player}! Enter (q)uit to exit the app at any time.\nLet's jump in!"
 
 # Quiz Component
 quiz = Quiz.new(player,10)
@@ -21,3 +30,10 @@ while quiz.quiz_finished? == false do
     quiz.check_answer(answer)
 end
 quiz.display_score
+
+# Exit Screen
+exit_banner = Artii::Base.new
+puts ""
+puts "---------------------------------THANKS FOR PLAYING---------------------------------"
+puts exit_banner.asciify(title)
+puts "------------------------------------------------------------------------------------"

d7a0e009b18f942ae9742e7b7a67a6b1c35fc7d2
t-hnh10 <t.hnh@tuta.io>
2019-04-24 15:28:02 +1000

    added loop in main app

--- main.rb
+++ main.rb
@@ -20,16 +20,25 @@ player = gets.strip
 slow_puts "\nWelcome #{player}! Enter (q)uit to exit the app at any time.\nLet's jump in!"
 
 # Quiz Component
-quiz = Quiz.new(player,10)
-while quiz.quiz_finished? == false do
-    quiz.display_question
-    answer = gets.strip.downcase
-        if answer[0] == "q"
-            break
-        end
-    quiz.check_answer(answer)
+readytoquit = false
+while readytoquit == false do
+    quiz = Quiz.new(player,10)
+    while quiz.quiz_finished? == false do
+        quiz.display_question
+        answer = gets.strip.downcase
+            if answer[0] == "q"
+                break
+            end
+        quiz.check_answer(answer)
+    end
+    quiz.display_score
+    puts "\nWould you like to play again? (y/n)"
+    response = gets.strip
+    if response.downcase == "n"
+        readytoquit = true
+    end
+    system "clear"
 end
-quiz.display_score
 
 # Exit Screen
 exit_banner = Artii::Base.new
